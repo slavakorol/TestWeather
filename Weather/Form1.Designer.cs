a31349b65ceee933c8b9e99c8e1bf5a204d65932
slavakorol <38197082+slavakorol@users.noreply.github.com>
2018-11-17 14:25:33 +1000

    Add files via upload

---
+++ Weather/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Weather
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(128, 195);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Покажи погоду";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_ClickAsync);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoCheck = false;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 201);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(109, 22);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "соединение";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(194, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 42);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "0°";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 42);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Сегодня";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 54);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(258, 134);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(113, 97);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(31, 18);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "0° -";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 97);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(66, 18);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Вторник";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(113, 61);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 18);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "0° -";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 61);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 18);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Понедельник";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(113, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 18);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "0° -";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 18);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Воскресенье";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(150, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 18);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "0°";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(150, 61);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(22, 18);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "0°";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(150, 97);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(22, 18);
+            this.label11.TabIndex = 1;
+            this.label11.Text = "0°";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 237);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Weather/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net.Http;
+using SvgNet;   // мб через нее можно конвертировать svg в png
+
+namespace Weather
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class WeatherInfo
+        {
+            public string now { get; set; }             // юникс тайм
+            public Fact fact{ get; set; }               // фактические значения
+            public Forecasts[] forecasts { get; set; }  // прогнозы
+        }
+        class Fact
+        {
+            public short temp { get; set; }             // температура
+            public string icon { get; set; }            // код иконки
+        }
+        class Forecasts
+        {
+            public string date { get; set; }            // дата прогноза 
+            public Parts parts { get; set; }            // части суток
+        }
+        class Parts
+        {
+            public Day_short day_short { get; set; }    // прогноз на день
+            public Night_short night_short { get; set; }// прогноз на ночь
+        }
+        class Day_short
+        {
+            public short temp { get; set; }            // максимальная темпа за день
+        }
+        class Night_short
+        {
+            public short temp { get; set; }            // минимальная темпа за ночь
+        }
+        private DateTime stringToDateTime(string date)
+        {
+            int year = Int32.Parse(date.Substring(0,4));    // получаем год
+            int month = Int32.Parse(date.Substring(5,2));   // месяц
+            int day = Int32.Parse(date.Substring(8,2));     // и день из json структуры
+            return new DateTime(year, month, day);
+        }
+
+        private async void button1_ClickAsync(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();                       // создаем клиента
+            HttpRequestMessage request = new HttpRequestMessage();      // создаем запрос для клиента
+
+            request.RequestUri = new Uri("https://api.weather.yandex.ru/v1/" +              // ссылка на апишку
+                "forecast?" +       // тестовый комплект услуг от Я.Погоды
+                "lat=48.48272&" +   // широта
+                "lon=135.08379&" +  // долгота
+                "limit=3&" +        // лимит прогноза
+                "extra=false");     // доп. инфа           
+            request.Method = HttpMethod.Get;                                                // хз че эт
+            request.Headers.Add("X-Yandex-API-Key", "dcaa56be-ef4e-458e-bec6-3390de3f977b");// имя заголовка и его значение (ключ из ЛК)
+
+            HttpResponseMessage response = await client.SendAsync(request); // кидаем запрос и ждем ответ
+            
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)        // чекаем код статуса
+            {
+                checkBox1.Checked = true;                                   // подтверждаем соединение
+                HttpContent responseContent = response.Content;             // получаем контент из ответа
+                var json = await responseContent.ReadAsStringAsync();       // считываем контент в json
+                var obj = JsonConvert.DeserializeObject<WeatherInfo>(json); // ковентируем json по маске класса WeatherInfo
+                
+                label1.Text = obj.fact.temp.ToString() + "°";                                           // сохраняем градусы в лейбл и добавляем символ градусов
+                //string imageLocation = "https://yastatic.net/weather/i/icons/blueye/color/svg/.svg";  // попытка сохранить svg в pictureBox
+                //imageLocation = imageLocation.Insert(54, obj.fact.icon);                              // вставляем код изображения погоды в ссылку
+                //pictureBox1.ImageLocation = imageLocation;                                            // осталось сконвертировать svg в png
+
+                //richTextBox1.Text = JsonConvert.DeserializeObject(json).ToString();
+
+                string[] days = new string[7] { "Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"};
+
+                // записываем прогноз на три дня вперед
+                label3.Text = days[(int)(stringToDateTime(obj.forecasts[0].date).DayOfWeek)];
+                label4.Text = obj.forecasts[0].parts.day_short.temp.ToString() + "°";
+                label5.Text = obj.forecasts[0].parts.night_short.temp.ToString() + "°";
+         
+                label6.Text = days[(int)(stringToDateTime(obj.forecasts[1].date).DayOfWeek)];
+                label7.Text = obj.forecasts[1].parts.day_short.temp.ToString() + "°";
+                label8.Text = obj.forecasts[1].parts.night_short.temp.ToString() + "°";
+
+                label9.Text = days[(int)(stringToDateTime(obj.forecasts[2].date).DayOfWeek)];
+                label10.Text = obj.forecasts[2].parts.day_short.temp.ToString() + "°";
+                label11.Text = obj.forecasts[2].parts.night_short.temp.ToString() + "°";
+            }
+            else
+            {
+                checkBox1.Checked = false;
+                MessageBox.Show("Нет соединения");
+            }
+        }
+    }
+}
